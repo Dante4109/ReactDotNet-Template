d6b11b4b21ca7fa981f10564f85901eee8341766
R3Z <rogerjohnmorellizeller@gmail.com>
2025-01-08 21:14:19 -0500

    converted getsinglequestion in IDataRepository to task

--- backend/api/Data/IDataRepository.cs
+++ backend/api/Data/IDataRepository.cs
@@ -14,7 +14,7 @@ namespace api.Data
         IEnumerable<QuestionGetManyResponse> GetQuestionsBySearchWithPaging(string search, int pageNumber, int pageSize);
         IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
         Task<IEnumerable<QuestionGetManyResponse>> GetUnansweredQuestionsAsync();
-        QuestionGetSingleResponse GetQuestion(int questionId); 
+        Task<QuestionGetSingleResponse> GetQuestion(int questionId);
         bool QuestionExists(int questionId);
         AnswerGetResponse GetAnswer(int answerId);
         QuestionGetSingleResponse PostQuestion(QuestionPostFullRequest question);
--- backend/api/Data/Models/User.cs
+++ backend/api/Data/Models/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QandA.Data.Models
+namespace api.Data.Models
 {
     public class User
     {
